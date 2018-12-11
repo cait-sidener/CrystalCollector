ac1753d9e068116b3400cc2f99ea60fb735e80f7
Cait Sidener <cait.sidener@gmail.com>
2018-12-10 19:35:55 -0600

    edited jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,98 @@
- $(document).ready(function(){
- 
- var targetNumber = function randomNumber() {
-      return Math.floor((Math.random() * 100) +1);
-  };
+ $(document).ready(function () {
 
-  $("#number-to-guess").text(targetNumber);
+     var targetNumber = Math.floor((Math.random() * 100) + 1);
 
-  var num1 = Math.floor(Math.random() * 12 +1);
-  var num2 = Math.floor(Math.random() * 12 +1);
-  var num3 = Math.floor(Math.random() * 12 +1);
-  var num4 = Math.floor(Math.random() * 12 +1);
+     //Sets up random numbers for each jewel between 1-12
+     var num1 = Math.floor(Math.random() * 12 + 1);
+     var num2 = Math.floor(Math.random() * 12 + 1);
+     var num3 = Math.floor(Math.random() * 12 + 1);
+     var num4 = Math.floor(Math.random() * 12 + 1);
 
-  var userTotal = 0;
-  var totalWins = 0;
-  var totalLosses = 0;
+     var userTotal = 0;
+     var totalWins = 0;
+     var totalLosses = 0;
 
-  $("#totalWins").text(totalWins);
-  $("#totalLosses").text(totalLosses);
+     $("#number-to-guess").text(targetNumber);
 
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
+     $("#totalWins").text(totalWins);
+     $("#totalLosses").text(totalLosses);
 
 
-    function reset() {
-        var targetNumber = function randomNumber() {
-            return Math.floor((Math.random() * 100) +1);
-        };
-        $("#number-to-guess").text(targetNumber);
-        var num1 = Math.floor(Math.random() * 12 +1);
-        var num2 = Math.floor(Math.random() * 12 +1);
-        var num3 = Math.floor(Math.random() * 12 +1);
-        var num4 = Math.floor(Math.random() * 12 +1);
-        userTotal = 0;
-    }
+     function reset() {
+         targetNumber = function randomNumber() {
+             return Math.floor((Math.random() * 100) + 1);
+         };
+         $("#number-to-guess").text(targetNumber);
+         num1 = Math.floor(Math.random() * 12 + 1);
+         num2 = Math.floor(Math.random() * 12 + 1);
+         num3 = Math.floor(Math.random() * 12 + 1);
+         num4 = Math.floor(Math.random() * 12 + 1);
+         userTotal = 0;
+         $("#totalPoints").text(userTotal);
+     }
 
-    function winner() {
-        alert("You win!");
-        totalWins++;
-        $("#totalWins").text(totalWins);
-        reset();
-    }
+     //Adds wins to userTotal
+     function winner() {
+         alert("You win!");
+         totalWins++;
+         $("#totalWins").text(totalWins);
+         reset();
+     }
 
-    function loser() {
-      alert("You lose!!");
-      totalLosses++;
-      $("#totalLosses").text(totalLosses);
-      reset();
-    }
+     //Adds losses to userTotal
+     function loser() {
+         alert("You lose!!");
+         totalLosses++;
+         $("#totalLosses").text(totalLosses);
+         reset();
+     }
 
+     // Our click event applies to jewels
+     $("#crystal1").on("click", function onClick() {
+         userTotal = userTotal + num1;
 
+         $("#totalPoints").text(userTotal, targetNumber);
 
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  $("#crystal1").on("click", function() {
-    userTotal = userTotal + num1;
-    $("totalPoints").text(userTotal);
+         console.log('onClick', userTotal, targetNumber);
 
-    if (userTotal === targetNumber) {
-        winner()
-    } else if (userTotal > targetNumber) {
-        loser()
-    }
-});
-    $("#crystal2").on("click", function() {
-        userTotal = userTotal + num2;
-        $("totalPoints").text(userTotal);
-    
-        if (userTotal === targetNumber) {
-            winner()
-        } else if (userTotal > targetNumber) {
-            loser()
-        }
-    });
-        $("#crystal3").on("click", function() {
-            userTotal = userTotal + num3;
-            $("totalPoints").text(userTotal);
-        
-            if (userTotal === targetNumber) {
-                winner()
-            } else if (userTotal > targetNumber) {
-                loser()
-            }
-        });
-            $("#crystal4").on("click", function() {
-                userTotal = userTotal + num4;
-                console.log("New userTotal= " + userTotal);
-                $("totalPoints").text(userTotal);
-            
-                if (userTotal === targetNumber) {
-                    winner()
-                } else if (userTotal > targetNumber) {
-                    loser()
-                }   
-        });
+         if (userTotal === targetNumber) {
+             winner()
+         } else if (userTotal > targetNumber) {
+             loser()
+         }
+     });
+     $("#crystal2").on("click", function () {
+         userTotal = userTotal + num2;
+         $("#totalPoints").text(userTotal);
 
-    });
+         if (userTotal === targetNumber) {
+             winner()
+         } else if (userTotal > targetNumber) {
+             loser()
+         }
+     });
+     $("#crystal3").on("click", function () {
+         userTotal = userTotal + num3;
+         $("#totalPoints").text(userTotal);
+
+         if (userTotal === targetNumber) {
+             winner()
+         } else if (userTotal > targetNumber) {
+             loser()
+         }
+     });
+     $("#crystal4").on("click", function () {
+         userTotal = userTotal + num4;
+
+         console.log("New userTotal= " + userTotal);
+         
+         $("#totalPoints").text(userTotal);
+
+         if (userTotal === targetNumber) {
+             winner()
+         } else if (userTotal > targetNumber) {
+             loser()
+         }
+     });
+
+ });
